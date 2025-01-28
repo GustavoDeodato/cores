f925c3a9eb259cac3baaa005d834be160d46c2aa
GUSTAVO SILVA DEODATO <24122736@CFP127ALUNO.LOCAL>
2025-01-28 15:58:22 -0300

    js ok

---
+++ main.js
@@ -0,0 +1,8 @@
+"use scrict"
+
+const botaoTrocarCor = document.getElementById('trocar-cor') 
+function trocaCor (){
+    const cor = document.getElementById('cor').value 
+    document.documentElement.style.setProperty('--cor-fundo', cor)
+}
+botaoTrocarCor.addEventListener('click', trocaCor) 
